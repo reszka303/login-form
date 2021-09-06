b14d967ef721b2936b0cf5f7dad654a16a2bfb9d
reszka303 <reszka303@op.pl>
2021-09-07 01:53:53 +0200

    Initial commit: Initial configuration

---
+++ src/main/java/com/github/loginform/client/home/HomeController.java
@@ -0,0 +1,15 @@
+package com.github.loginform.client.home;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet("")
+public class HomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+}
