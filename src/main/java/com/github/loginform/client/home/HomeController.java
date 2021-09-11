e5a908f89155215daa8eccdce0d927e3baabcae2
reszka303 <reszka303@op.pl>
2021-09-11 14:56:55 +0200

    2nd commit: home page template

--- src/main/java/com/github/loginform/client/home/HomeController.java
+++ src/main/java/com/github/loginform/client/home/HomeController.java
@@ -10,6 +10,6 @@ import java.io.IOException;
 public class HomeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
+        request.getRequestDispatcher("/index.html").forward(request, response);
     }
 }
