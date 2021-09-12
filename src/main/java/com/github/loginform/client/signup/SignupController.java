d5e3927583d44460c58d5bd6185e8e516c0d9aa4
reszka303 <reszka303@op.pl>
2021-09-12 17:30:21 +0200

    3rd commit: signup template

---
+++ src/main/java/com/github/loginform/client/signup/SignupController.java
@@ -0,0 +1,20 @@
+package com.github.loginform.client.signup;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet("/signup")
+public class SignupController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
