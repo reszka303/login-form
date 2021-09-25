f95811593928c4425726317ea92df80a226df25c
reszka303 <reszka303@op.pl>
2021-09-25 23:04:16 +0200

    12th commit: database configuration

---
+++ src/main/java/com/github/loginform/config/DataSourceProvider.java
@@ -0,0 +1,21 @@
+package com.github.loginform.config;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DataSourceProvider {
+    private static DataSource dataSource;
+
+    private DataSourceProvider() { }
+
+    public static DataSource getDataSource() throws NamingException {
+        if (dataSource == null) {
+            InitialContext initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            dataSource = (DataSource) envContext.lookup("jdbc/loginform");
+        }
+        return dataSource;
+    }
+}
---
+++ src/main/resources/loginform.sql
@@ -0,0 +1,7 @@
+# usuń bazę danych jeśli istnieje
+DROP DATABASE IF EXISTS loginform;
+# utwórz bazę danych i ustaw kodowanie znaków na polskie znaki
+CREATE DATABASE loginform CHARACTER SET utf8mb4 COLLATE utf8mb4_polish_ci;
+# użyj bazdę danych loginform w schema
+USE loginform;
+
